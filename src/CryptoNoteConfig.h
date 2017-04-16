2f50ecfef19f0d0439711d345939cc6ed27cba47
LeviarCoin <info@leviarcoin.org>
2017-04-16 20:42:21 +0200

    First commit

---
+++ src/CryptoNoteConfig.h
@@ -0,0 +1,126 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+#include <cstdint>
+#include <initializer_list>
+
+namespace CryptoNote {
+namespace parameters {
+
+const uint64_t CRYPTONOTE_MAX_BLOCK_NUMBER                   = 500000000;
+const size_t   CRYPTONOTE_MAX_BLOCK_BLOB_SIZE                = 500000000;
+const size_t   CRYPTONOTE_MAX_TX_SIZE                        = 1000000000;
+//LeviarCoin address prefix
+const uint64_t CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX       = 0x76;
+//Maturity period for your currency
+const size_t   CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW          = 60;
+const uint64_t CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT            = 60 * 60 * 2;
+
+const size_t   BLOCKCHAIN_TIMESTAMP_CHECK_WINDOW             = 60;
+
+//54 million coins (Max Supply)
+const uint64_t MONEY_SUPPLY                                  = UINT64_C(5400000000000000);
+const unsigned EMISSION_SPEED_FACTOR                         = 22;
+//2.7 million dev team + Buonties / 5.4 million Crowdfunding
+const uint64_t GENESIS_BLOCK_REWARD                          = UINT64_C(810000000000000);
+
+static_assert(EMISSION_SPEED_FACTOR <= 8 * sizeof(uint64_t), "Bad EMISSION_SPEED_FACTOR");
+
+const size_t   CRYPTONOTE_REWARD_BLOCKS_WINDOW               = 100;
+const size_t   CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE     = 20000; //size of block (bytes) after which reward for block calculated using block size
+const size_t   CRYPTONOTE_COINBASE_BLOB_RESERVED_SIZE        = 600;
+//Decimals
+const size_t   CRYPTONOTE_DISPLAY_DECIMAL_POINT              = 8;
+//Min-fee
+const uint64_t MINIMUM_FEE                                   = 1000;
+const uint64_t DEFAULT_DUST_THRESHOLD                        = MINIMUM_FEE;
+
+//Block's target time
+const uint64_t DIFFICULTY_TARGET                             = 60; // seconds
+const uint64_t EXPECTED_NUMBER_OF_BLOCKS_PER_DAY             = 24 * 60 * 60 / DIFFICULTY_TARGET;
+
+const size_t   DIFFICULTY_WINDOW                             = EXPECTED_NUMBER_OF_BLOCKS_PER_DAY; // blocks
+const size_t   DIFFICULTY_CUT                                = 60;  // timestamps to cut after sorting
+const size_t   DIFFICULTY_LAG                                = 15;
+static_assert(2 * DIFFICULTY_CUT <= DIFFICULTY_WINDOW - 2, "Bad DIFFICULTY_WINDOW or DIFFICULTY_CUT");
+
+const size_t   MAX_BLOCK_SIZE_INITIAL                        =  20 * 1024;
+const uint64_t MAX_BLOCK_SIZE_GROWTH_SPEED_NUMERATOR         = 100 * 1024;
+const uint64_t MAX_BLOCK_SIZE_GROWTH_SPEED_DENOMINATOR       = 365 * 24 * 60 * 60 / DIFFICULTY_TARGET;
+
+const uint64_t CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_BLOCKS     = 1;
+const uint64_t CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_SECONDS    = DIFFICULTY_TARGET * CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_BLOCKS;
+
+const uint64_t CRYPTONOTE_MEMPOOL_TX_LIVETIME                = 60 * 60 * 24;     //seconds, one day
+const uint64_t CRYPTONOTE_MEMPOOL_TX_FROM_ALT_BLOCK_LIVETIME = 60 * 60 * 24 * 7; //seconds, one week
+const uint64_t CRYPTONOTE_NUMBER_OF_PERIODS_TO_FORGET_TX_DELETED_FROM_POOL = 7;  // CRYPTONOTE_NUMBER_OF_PERIODS_TO_FORGET_TX_DELETED_FROM_POOL * CRYPTONOTE_MEMPOOL_TX_LIVETIME = time to forget tx
+
+const size_t   FUSION_TX_MAX_SIZE                            = CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE * 30 / 100;
+const size_t   FUSION_TX_MIN_INPUT_COUNT                     = 12;
+const size_t   FUSION_TX_MIN_IN_OUT_COUNT_RATIO              = 4;
+
+const char     CRYPTONOTE_BLOCKS_FILENAME[]                  = "blocks.dat";
+const char     CRYPTONOTE_BLOCKINDEXES_FILENAME[]            = "blockindexes.dat";
+const char     CRYPTONOTE_BLOCKSCACHE_FILENAME[]             = "blockscache.dat";
+const char     CRYPTONOTE_POOLDATA_FILENAME[]                = "poolstate.bin";
+const char     P2P_NET_DATA_FILENAME[]                       = "p2pstate.bin";
+const char     CRYPTONOTE_BLOCKCHAIN_INDICES_FILENAME[]      = "blockchainindices.dat";
+const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json";
+} // parameters
+
+const char     CRYPTONOTE_NAME[]                             = "leviarcoin";
+// Genesis TxHex
+// NOTE: THIS WILL BE RELEASED AFTER ICO
+const char     GENESIS_COINBASE_TX_HEX[]                     = "rel-after-ico";
+
+const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
+const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
+const uint8_t  BLOCK_MINOR_VERSION_0                         =  0;
+
+const size_t   BLOCKS_IDS_SYNCHRONIZING_DEFAULT_COUNT        =  10000;  //by default, blocks ids count in synchronizing
+const size_t   BLOCKS_SYNCHRONIZING_DEFAULT_COUNT            =  200;    //by default, blocks count in blocks downloading
+const size_t   COMMAND_RPC_GET_BLOCKS_FAST_MAX_COUNT         =  1000;
+
+const int      P2P_DEFAULT_PORT                              = 18001;
+const int      RPC_DEFAULT_PORT                              = 19001;
+const int      DRM_NETWORK_DEFAULT_PORT                      = 19002;
+
+const size_t   P2P_LOCAL_WHITE_PEERLIST_LIMIT                =  1000;
+const size_t   P2P_LOCAL_GRAY_PEERLIST_LIMIT                 =  5000;
+
+const size_t   P2P_CONNECTION_MAX_WRITE_BUFFER_SIZE          = 16 * 1024 * 1024; // 16 MB
+const uint32_t P2P_DEFAULT_CONNECTIONS_COUNT                 = 8;
+const size_t   P2P_DEFAULT_WHITELIST_CONNECTIONS_PERCENT     = 70;
+const uint32_t P2P_DEFAULT_HANDSHAKE_INTERVAL                = 60;            // seconds
+const uint32_t P2P_DEFAULT_PACKET_MAX_SIZE                   = 50000000;      // 50000000 bytes maximum packet size
+const uint32_t P2P_DEFAULT_PEERS_IN_HANDSHAKE                = 250;
+const uint32_t P2P_DEFAULT_CONNECTION_TIMEOUT                = 5000;          // 5 seconds
+const uint32_t P2P_DEFAULT_PING_CONNECTION_TIMEOUT           = 2000;          // 2 seconds
+const uint64_t P2P_DEFAULT_INVOKE_TIMEOUT                    = 60 * 2 * 1000; // 2 minutes
+const size_t   P2P_DEFAULT_HANDSHAKE_INVOKE_TIMEOUT          = 5000;          // 5 seconds
+const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1510d13336228debfee9c918ce505efe225d8c94d045fa115";
+
+const std::initializer_list<const char*> SEED_NODES = {
+  "leviarcoin.org:18001",
+};
+
+struct CheckpointData {
+  uint32_t height;
+  const char* blockId;
+};
+
+#ifdef __GNUC__
+__attribute__((unused))
+#endif
+
+// You may add here other checkpoints using the following format:
+// {<block height>, "<block hash>"},
+const std::initializer_list<CheckpointData> CHECKPOINTS = {
+  //{ 10000, "84b6345731e2702cdaadc6ce5e5238c4ca5ecf48e3447136b2ed829b8a95f3ad" },
+};
+} // CryptoNote
+
+#define ALLOW_DEBUG_COMMANDS
---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x65, 0xa9, 0x92, 0x20, 0xf9, 0x2c, 0x1a, 0x34, 0x8b, 0x5e, 0xc0, 0x3b, 0x12, 0x62, 0x82, 0x19 } };
+}
